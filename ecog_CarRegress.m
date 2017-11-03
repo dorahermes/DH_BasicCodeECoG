97fd421655e004cd4b2b0134d749a17480cc9c06
Dora <dora@Doras-MacBook-Pro.local>
2017-11-03 10:11:27 -0700

    added NYU code and updates for filetring

--- ecog_CarRegress.m
+++ ecog_CarRegress.m
@@ -5,24 +5,19 @@ function [signal] = ecog_CarRegress(signal, chans2incl)
 % dh - Oct 2010
 % signal = electrodes X samples
 
-if size(signal,1) < size(signal,2) % signal samples X electrodes
-    disp('transpose signal to be samples X electrodes')
-    return
-end
-
 if size(signal,2) < size(signal,1) % signal samples X electrodes
     disp('transpose signal to be electrodes X samples')
     return
 end
 
-ca_signal = mean(signal(:,chans2incl),2);
+ca_signal = mean(signal(chans2incl,:),1);
 
 % regress off the mean signal
-for k = 1:size(signal,2) % elecs
-    disp(['elec ' int2str(k)])
-    if ismember(k,chans2incl)
-        [B,BINT,R] = regress(signal(:,k),ca_signal);
-        signal(:,k)=R';
+for kk = 1:size(signal,1) % elecs
+    disp(['elec ' int2str(kk)])
+    if ismember(kk,chans2incl)
+        [B,BINT,R] = regress(signal(kk,:)',ca_signal');
+        signal(kk,:)=R';
     end
 end
 
